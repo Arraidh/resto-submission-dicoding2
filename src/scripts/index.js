2222f0bf95ea223fd17af7122d913d9fb2c8e983
Arraidh <ari.darmika@gmail.com>
2023-05-15 18:55:56 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,42 @@
+import "regenerator-runtime";
+import "../styles/main.scss";
+import data from "../DATA.json";
+
+console.log("Hello Coders! :)");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+
+const restoData = { ...data.restaurants };
+const cardData = { ...restoData };
+
+const cardWrapper = document.getElementById("card-wrapper");
+
+for (let card = 0; card < Object.keys(cardData).length; card++) {
+  cardWrapper.innerHTML += `<div class="card">
+  <div class="head-card">
+    <p class="body-city" tabindex="${card}">
+    ${cardData[card].city}
+    </p>
+    <img src="${cardData[card].pictureId}" alt="" tabindex="${card}"/>
+    <div class="body-rating">
+      <img src="./images/Rating.png" alt="Picture of ${cardData[card].name}" />
+      <p tabindex="${card}">
+        ${cardData[card].rating}
+      </p>
+    </div>
+  </div>
+  <div class="body-card">
+    <h1 tabindex="${card + 1}">${cardData[card].name}</h1>
+    <p tabindex="${card}">
+      ${cardData[card].description}
+    </p>
+  </div>
+</div>`;
+}
---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src/scripts/index.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/templates/index.html"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/public/"),
+          to: path.resolve(__dirname, "dist/"),
+        },
+      ],
+    }),
+  ],
+};
